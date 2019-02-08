ef9583f1d2051a35673e56133373676ca109c334
Amanda Murphy <helloamandamurphy@gmail.com>
2019-02-08 21:58:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function overWriteBestCustomer() {
 const leastFavoriteCustomer = "glenda"
 
 function changeLeastFavoriteCustomer() {
-  leastFavoriteCustomer = "gildy"
+  leastFavoriteCustomer = "gildy";
 }
